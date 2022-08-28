a23b60636cfcf8fe14d801115b2121cfbc2cc6bf
Дмитрий Бурмистров <supamem74@gmail.com>
2022-08-28 23:44:52 +0300

    feature-preform-template-presenter-component

---
+++ src/components/TemplatePresenter/TemplatePresenter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TemplatePresenter = () => {
+	return (
+		<div style={{
+			flexGrow: "1"
+		}}>
+			<h1>Template presenter</h1>
+		</div>
+	)
+}
+
+export default TemplatePresenter
